27d2abc49c55fe73a4647c6bbd92823d9a13dd3c
InaYarashevich <ina.yarashevich@gmail.com>
2022-03-02 21:56:22 +0300

    stringtask4.3

--- src/main/java/tasks/homework/stringtask/StringTask4.java
+++ src/main/java/tasks/homework/stringtask/StringTask4.java
@@ -1,8 +1,16 @@
 package main.java.tasks.homework.stringtask;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
 
 public class StringTask4 {
 
-    public static void main(String[] args) {
+    DateFormat dateFormat = new SimpleDateFormat("hh.mm dd.MM.yyyy");
+    String text = dateFormat.format(new Date());
 
+    public void printDate(String text){
+
+        System.out.println();
     }
 }

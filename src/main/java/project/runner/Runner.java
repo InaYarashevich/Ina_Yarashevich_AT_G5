9dcb3c2382c8dc06f34db2e0e538981dbc6a58ce
InaYarashevich <ina.yarashevich@gmail.com>
2022-03-05 10:27:27 +0300

    updating Bubbles

--- src/main/java/project/runner/Runner.java
+++ src/main/java/project/runner/Runner.java
@@ -3,14 +3,16 @@ package main.java.project.runner;
 import main.java.project.boxing.Bottle;
 
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         Bottle bottle = new Bottle(0.5);
         Bottle bottle2 = new Bottle(1);
         Bottle bottle3 = new Bottle(1.5);
 
-//        bottle.open();
-//        bottle2.open();
-//        bottle3.open();
+        bottle.open();
+        System.out.println();
+        bottle2.open();
+        System.out.println();
+        bottle3.open();
     }
 }

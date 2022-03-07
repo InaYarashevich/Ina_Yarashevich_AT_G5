22f6a9fb3e98e2d646d4d53708c6288cf5b359ac
InaYarashevich <ina.yarashevich@gmail.com>
2022-03-07 20:07:29 +0300

    updated ip_address task

--- src/main/java/tasks/homework/stringtask/StringTask2.java
+++ src/main/java/tasks/homework/stringtask/StringTask2.java
@@ -1,21 +1,18 @@
 package main.java.tasks.homework.stringtask;
 
-import java.util.Arrays;
-
 public class StringTask2 {
 
     public static void main(String[] args) {
 
-        String task1 = "asdf1 word 234 563ad word";
-        String s = task1.replaceAll("[^\\d]", " ");
-        s = s.trim();
-        s = s.replaceAll(" +", "");
-        String[] array2 = s.split("");
-        int[] intArray = new int[array2.length];
+        String randomString = "asdf1 word 234 563ad word";
+        String newString = randomString.replaceAll("[\\D+]", " ").trim();
+        newString = newString.replaceAll(" +", "");
+        String[] array = newString.split("");
+        String[] numbersArray = new String[array.length];
 
-        for (int i = 0; i < array2.length; i++) {
-            intArray[i] = Integer.parseInt(array2[i]);
+        for (int i = 0; i < array.length; i++) {
+            numbersArray[i] = array[i];
+            System.out.print(numbersArray[i]);
         }
-        System.out.println(Arrays.toString(intArray));
     }
 }
--- src/main/java/tasks/homework/stringtask/StringTask3.java
+++ src/main/java/tasks/homework/stringtask/StringTask3.java
@@ -1,5 +1,6 @@
 package main.java.tasks.homework.stringtask;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class StringTask3 {
@@ -10,11 +11,27 @@ public class StringTask3 {
                 "access_log.2020.09.07 212.168.101.5 granted\n" +
                 "access_log.2020.09.07 212.168.101.6 denied";
 
-        String[] initialArray = logs.split("\n");
-        String[][] newArray = new String[initialArray.length][3];
+
+        String newLogs = logs.replaceAll("access_log.[0-9]{4}.[0-9]{2}.[0-9]{2}", "");
+        String removedOk = newLogs.replaceAll("granted", "");
+        String removedFailed = removedOk.replaceAll("denied", "").trim().replaceAll(" +", "");
+        String[] initialArray = removedFailed.split("\n");
+        String[] newArray = new String[initialArray.length];
+        String grantedCount = "granted";
+        String deniedCount = "denied";
+        String uniqueIp;
 
         for (int i = 0; i < initialArray.length; i++) {
-            newArray[i] = initialArray[i].split(" ");
+            //uniqueIp = initialArray[i];
+            for (int k = 0; k < newArray.length; k++){
+                if (!newArray[k].equals(initialArray[i])) {
+                    newArray[k] = initialArray[i];
+                }
+            }
+        }
+        System.out.println(Arrays.toString(newArray));
+        for (String s : newArray) {
+            System.out.println(s);
         }
     }
 }

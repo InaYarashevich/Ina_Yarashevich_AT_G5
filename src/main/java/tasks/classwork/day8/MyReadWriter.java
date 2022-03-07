6d4be64e822465da92bdfbc4ff8a6976d092ddf6
InaYarashevich <ina.yarashevich@gmail.com>
2022-03-07 18:09:03 +0300

    added files from classwork

--- src/main/java/tasks/classwork/day8/MyReadWriter.java
+++ src/main/java/tasks/classwork/day8/MyReadWriter.java
@@ -1,12 +1,10 @@
 package main.java.tasks.classwork.day8;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class MyReadWriter {
 
-    public static void read() throws IOException {
+    public void read() throws IOException {
 
         BufferedReader in = new BufferedReader(new FileReader("file.txt"));
         String line;
@@ -14,5 +12,26 @@ public class MyReadWriter {
             System.out.println(line);
         }
         in.close();
+    }
+
+    public void write() throws IOException {
+
+        BufferedWriter out = new BufferedWriter(new FileWriter("new_file.txt"));
+        out.write("мама мыла раму");
+        out.close();
+    }
+
+    public void  copyText(String fileFrom, String fileTo) throws IOException{
+
+        BufferedReader in = new BufferedReader(new FileReader(fileFrom));
+        BufferedWriter out = new BufferedWriter(new FileWriter(fileTo));
+
+        String line;
+        while ((line = in.readLine()) != null){
+            out.append(line);
+            out.newLine();
+        }
+        in.close();
+        out.close();
     }
 }

f081813c05f23d0e9e0b0270bcd8246f0892b83c
InaYarashevich <ina.yarashevich@gmail.com>
2022-03-10 07:11:04 +0300

    updated ip_address task

--- src/main/java/tasks/homework/stringtask/IpAddress.java
+++ src/main/java/tasks/homework/stringtask/IpAddress.java
@@ -1,19 +1,17 @@
 package main.java.tasks.homework.stringtask;
 
-import java.util.Arrays;
-
 public class IpAddress {
 
     public static void main(String[] args) {
         String logs = "access_log.2020.09.07 212.168.101.5 granted\n" +
                 "access_log.2020.09.07 212.168.101.6 denied\n" +
                 "access_log.2020.09.07 212.168.101.5 granted\n" +
+                "access_log.2020.09.07 212.168.101.6 denied\n" +
                 "access_log.2020.09.07 212.168.101.6 denied";
 
-
         String newLogs = logs.replaceAll("access_log.[0-9]{4}.[0-9]{2}.[0-9]{2}", "");
-        String removedOk = newLogs.replaceAll("granted", "");
-        String removedFailed = removedOk.replaceAll("denied", "").trim().replaceAll(" +", "");
+        String removedGranted = newLogs.replaceAll("granted", "");
+        String removedFailed = removedGranted.replaceAll("denied", "").trim().replaceAll(" +", "");
         String[] initialArray = removedFailed.split("\n");
         String[] newArray = new String[initialArray.length];
         String grantedCount = "granted";
@@ -21,14 +19,13 @@ public class IpAddress {
         String uniqueIp;
 
         for (int i = 0; i < initialArray.length; i++) {
-            //uniqueIp = initialArray[i];
-            for (int k = 0; k < newArray.length; k++){
-                if (!newArray[k].equals(initialArray[i])) {
-                    newArray[k] = initialArray[i];
+            for (int k = i + 1; k < initialArray.length; k++){
+                if (!initialArray[i].equals(initialArray[k])) {
+                    newArray[i] = initialArray[i];
                 }
             }
         }
-        System.out.println(Arrays.toString(newArray));
+
         for (String s : newArray) {
             System.out.println(s);
         }

9a27ab2fe39e0ee1096044dcf83acc180d42f460
InaYarashevich <ina.yarashevich@gmail.com>
2022-03-13 14:06:17 +0300

    updated Runner

--- src/main/java/project/runner/Runner.java
+++ src/main/java/project/runner/Runner.java
@@ -1,14 +1,13 @@
 package main.java.project.runner;
 
+import main.java.project.material.materials.Glass;
 import main.java.project.vessel.boxing.Bottle;
 
 public class Runner {
     public static void main(String[] args) throws InterruptedException {
 
-
-
-       /*  Bottle bottle = new Bottle(0.5, 0.05, 500, );
-       Bottle bottle2 = new Bottle(1);
+         Bottle bottle = new Bottle(0.5, 0.05, 500, new Glass(1.2, "qwe", 1.2));
+       /* Bottle bottle2 = new Bottle(1);
         Bottle bottle3 = new Bottle(1.5);*/
 
        /*  bottle.open();

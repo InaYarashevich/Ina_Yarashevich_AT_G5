6044a6b8ef2324216ea209830dcbb415f39ee9af
InaYarashevich <ina.yarashevich@gmail.com>
2022-03-14 08:25:04 +0300

    task2

---
+++ src/main/java/tasks/homework/arraylisttasks/Figures.java
@@ -0,0 +1,36 @@
+package main.java.tasks.homework.arraylisttasks;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Figures {
+
+    public static void main(String[] args) {
+
+        List<String> figures = Arrays.asList("Овал", "Прямоугольник", "Круг", "Квадрат", "Эллипс");
+
+        for (String figure : figures) {
+            System.out.print(figure + "-");
+        }
+        System.out.println();
+
+        int counter = 0;
+        for (String figure : figures) {
+            if (!figure.contains("и")) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
+
+        for (int i = 0; i < figures.size(); i++) {
+            System.out.print(figures.get(i) + " ");
+        }
+        System.out.println();
+
+        //figures.add(3, "Треугольник");
+        figures.set(3, "Треугольник");
+        for (String figure : figures) {
+            System.out.print(figure + " ");
+        }
+    }
+}

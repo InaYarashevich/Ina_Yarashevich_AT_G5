fbd3ac44c4d583ac0ec66ea78e4d17b70df879ea
InaYarashevich <ina.yarashevich@gmail.com>
2022-03-14 08:37:51 +0300

    task3

---
+++ src/main/java/tasks/homework/arraylisttasks/Cities.java
@@ -0,0 +1,30 @@
+package main.java.tasks.homework.arraylisttasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cities {
+
+    public static void main(String[] args) {
+
+        List<String> cities = new ArrayList<>();
+
+        cities.add("Минск");
+        cities.add("Москва");
+        cities.add("Берлин");
+
+        for (String city: cities){
+            System.out.println(city);
+        }
+
+        int counter = 0;
+        for (String city: cities){
+            counter += city.length();
+        }
+        System.out.println(counter);
+
+        for (int i = 0; i < cities.size(); i++){
+            System.out.print(cities.get(i) + " ");
+        }
+    }
+}

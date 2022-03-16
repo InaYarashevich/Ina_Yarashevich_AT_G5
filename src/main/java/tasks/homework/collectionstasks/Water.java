631e6ba66985e510417cbd594fa8d366ba12a532
InaYarashevich <ina.yarashevich@gmail.com>
2022-03-16 07:51:09 +0300

    task13

---
+++ src/main/java/tasks/homework/collectionstasks/Water.java
@@ -0,0 +1,28 @@
+package main.java.tasks.homework.collectionstasks;
+
+public class Water {
+
+    private String color;
+    private String smell;
+
+    public Water(String color, String smell) {
+        this.color = color;
+        this.smell = smell;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getSmell() {
+        return smell;
+    }
+
+    public void setSmell(String smell) {
+        this.smell = smell;
+    }
+}

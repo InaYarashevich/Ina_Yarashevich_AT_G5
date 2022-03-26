147f4b552f97dbc7059f5d07b7827445f2231b23
InaYarashevich <ina.yarashevich@gmail.com>
2022-03-26 10:23:30 +0300

    person collection task updated

--- src/main/java/tasks/homework/streamtasks/PersonCollection.java
+++ src/main/java/tasks/homework/streamtasks/PersonCollection.java
@@ -1,7 +1,9 @@
 package main.java.tasks.homework.streamtasks;
 
 import java.io.IOException;
+import java.util.Comparator;
 import java.util.Random;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class PersonCollection {
@@ -10,10 +12,17 @@ public class PersonCollection {
 
         Random random = new Random();
 
-
-        Stream.generate(() -> new Person("name", "surname", random.nextInt(30)))
+        Stream.generate(() -> new Person("name" + random.nextInt(100),
+                        "surname" + random.nextInt(100),
+                        random.nextInt(30)))
                 .limit(100)
+                .filter(person -> person.getAge() < 21)
+                .peek(person -> System.out.println(person.getName() + " " + person.getAge()))
+                .sorted(Comparator.comparing(Person::getSurname)
+                        .thenComparing(Person::getName))
+                .limit(4)
+                .map(Person::getName)
+                .collect(Collectors.toList())
                 .forEach(System.out::println);
-
     }
 }

62a645368e0956da63e8595b9b59d51efe714274
InaYarashevich <ina.yarashevich@gmail.com>
2022-03-28 07:16:25 +0300

    classwork day 13

---
+++ src/main/java/tasks/classwork/day13/Blocks.java
@@ -0,0 +1,33 @@
+package main.java.tasks.classwork.day13;
+
+public class Blocks {
+
+    int x;
+    static int y;
+/*
+
+     {
+        method();
+    }
+
+    public void method(){
+        System.out.println("I am public static void method");
+    }
+*/
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public  int getY() {
+        return y;
+    }
+
+    public  void setY(int y) {
+        Blocks.y = y;
+    }
+}

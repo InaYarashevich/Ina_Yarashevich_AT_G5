495d31d80a0356a9a4762377f03a7d038600f7e8
InaYarashevich <ina.yarashevich@gmail.com>
2022-03-28 09:04:20 +0300

    bubbles updating

--- src/main/java/project/vessel/Bottle.java
+++ src/main/java/project/vessel/Bottle.java
@@ -4,6 +4,7 @@ import main.java.project.material.Material;
 import main.java.project.stuff.Transformable;
 import main.java.project.stuff.Bubble;
 import main.java.project.stuff.SparklingWater;
+import main.java.project.stuff.Water;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,27 +13,29 @@ public class Bottle extends Vessel implements Containable {
 
     private double volume;
     private Transformable stuff;
-    SparklingWater water = new SparklingWater("no", "transparent", "no", 5);
+
+    SparklingWater sparklingWater = new SparklingWater("no", "transparent", "no", 5);
 
     public Bottle(double volume, double diameter, Material material) {
         super(volume, diameter, material);
-        this.setDiameter(2.0);
+        setDiameter(2.0);
         List<Bubble> bubbles = new ArrayList<>();
         for (int i = 0; i < 10000 * volume; i++) {
             bubbles.add(new Bubble("CO2"));
         }
-        this.water.pump(bubbles);
+        sparklingWater.pump(bubbles);
         System.out.println("Bottle is pumped with sparkling water.");
     }
 
+    @Override
     public void warm(int temperature) {
-        this.water.setTemperature(temperature);
+        sparklingWater.setTemperature(temperature);
         System.out.printf("Warming water to: %s", temperature + " ").println();
     }
 
     @Override
     public void addStuff(Transformable stuff) {
-
+        this.sparklingWater = (SparklingWater) stuff;
     }
 
     @Override
@@ -51,21 +54,13 @@ public class Bottle extends Vessel implements Containable {
     }
 
     public void open() {
-        this.water.setOpened(true);
+        sparklingWater.setOpened(true);
         System.out.print("Open the bottle ");
     }
 
     @Override
     public void close() {
 
-    }
-
-    public SparklingWater getWater() {
-        return water;
-    }
-
-    public void setWater(SparklingWater water) {
-        this.water = water;
     }
 
     public double getVolume() {

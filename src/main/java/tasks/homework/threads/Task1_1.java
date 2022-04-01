15229a87e15f1248dc7a662dfc440f6af0e5866e
InaYarashevich <ina.yarashevich@gmail.com>
2022-04-01 07:53:07 +0300

    completed task 1.2

--- src/main/java/tasks/homework/threads/Task1_1.java
+++ src/main/java/tasks/homework/threads/Task1_1.java
@@ -1,21 +1,40 @@
 package main.java.tasks.homework.threads;
 
-import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Task1_1 {
 
     public static void main(String[] args) {
 
-        Random random = new Random();
+        Thread t1 = new Thread(() -> {
+            IntStream.rangeClosed(1, 20).boxed().map(Mouse::new)
+                    .forEach(Mouse::peep);
+        });
 
-        new Thread(() -> {
-            for (int i = 0; i < 5; i++) {
-                System.out.printf("thread-%s", i);
-                System.out.println();
-                IntStream.rangeClosed(1, 20).boxed().map(Mouse::new)
-                        .forEach(Mouse::peep);
-            }
-        }).start();
+        Thread t2 = new Thread(() -> {
+            IntStream.rangeClosed(1, 20).boxed().map(Mouse::new)
+                    .forEach(Mouse::peep);
+        });
+
+        Thread t3 = new Thread(() -> {
+            IntStream.rangeClosed(1, 20).boxed().map(Mouse::new)
+                    .forEach(Mouse::peep);
+        });
+
+        Thread t4 = new Thread(() -> {
+            IntStream.rangeClosed(1, 20).boxed().map(Mouse::new)
+                    .forEach(Mouse::peep);
+        });
+
+        Thread t5 = new Thread(() -> {
+            IntStream.rangeClosed(1, 20).boxed().map(Mouse::new)
+                    .forEach(Mouse::peep);
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
     }
 }

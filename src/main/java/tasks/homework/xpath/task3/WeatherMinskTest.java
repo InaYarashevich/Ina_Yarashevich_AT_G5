1bfa2a5f4270bfecb3631b3bd6e9c94f8e27750d
InaYarashevich <ina.yarashevich@gmail.com>
2022-04-14 06:57:55 +0300

    refactoring WeatherMinsk Task

--- src/main/java/tasks/homework/xpath/task3/WeatherMinskTest.java
+++ src/main/java/tasks/homework/xpath/task3/WeatherMinskTest.java
@@ -6,11 +6,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class WeatherMinskTest {
 
     static WebDriver driver = new ChromeDriver();
 
     public static void main(String[] args) {
+
+        DateFormat dtf = new SimpleDateFormat("HH");
+        String now = dtf.format(new Date());
 
         driver.get("https://google.com");
         driver.findElement(By.name("q")).sendKeys("погода Минск");
@@ -18,8 +25,15 @@ public class WeatherMinskTest {
         el.sendKeys(Keys.ARROW_DOWN);
         el.sendKeys(Keys.ENTER);
         driver.findElement(By.xpath("//div[@id='wob_dp']/div[@data-wob-di='1']")).click();
-        System.out.println(driver
-                .findElement(By.xpath("//*[contains( normalize-space(@aria-label), '12:00') and contains(@aria-label,'Celsius')][1]"))
-                .getAttribute("aria-label"));
+
+        if (Integer.parseInt(now) > 12) {
+            System.out.println(driver
+                    .findElement(By.xpath("//*[contains( normalize-space(@aria-label), '12:00') and contains(@aria-label,'Celsius')][1]"))
+                    .getAttribute("aria-label"));
+        } else {
+            System.out.println(driver
+                    .findElement(By.xpath("//*[contains( normalize-space(@aria-label), '12:00') and contains(@aria-label,'Celsius')][2]"))
+                    .getAttribute("aria-label"));
+        }
     }
 }
